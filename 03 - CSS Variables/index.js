413ff11c4052c8081ef7c8f9d221adb9e75c0318
Ovidio Pérez <oapm10@gmail.com>
2021-05-01 20:43:13 -0400

    Finish project 3 (CSS Variables)

--- 03 - CSS Variables/index.js
+++ 03 - CSS Variables/index.js
@@ -4,8 +4,23 @@ window.addEventListener("DOMContentLoaded", function () {
             .getElementsByClassName("options-container")[0]
             .querySelectorAll("input")
     );
-    const documentStyles = getComputedStyle(document.documentElement);
-    const blur = documentStyles.getPropertyValue("--blur");
-    const spacing = documentStyles.getPropertyValue("--spacing");
-    const base = documentStyles.getPropertyValue("--base");
+
+    const documentStyles = document.documentElement.style;
+
+    inputsElements.forEach((input) => {
+        input.addEventListener("input", updateCssVariables, false);
+        input.addEventListener("change", updateCssVariables, false);
+    });
+
+    function updateCssVariables(e) {
+        const inputValue = e.target.value;
+        const inputId = e.target.id;
+
+        if (e.target.type == "color") {
+            documentStyles.setProperty("--base", `${inputValue}`);
+        }
+        if (e.target.type == "range") {
+            documentStyles.setProperty(`--${inputId}`, `${inputValue}px`);
+        }
+    }
 });
