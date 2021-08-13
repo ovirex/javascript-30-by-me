519501c4537060f22cd79ca93f277d8ebf476b6e
Ovidio Pérez <oapm10@gmail.com>
2021-08-13 16:25:08 -0400

    Finish project 7 (Array Cardio Day 2)

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -18,11 +18,39 @@ const comments = [
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
 // Array.prototype.every() // is everyone 19 or older?
+const actualYear = new Date().getFullYear();
+
+const isAtLeast = people.some((person) => actualYear - person.year >= 19);
+const isEveryone = people.every((person) => actualYear - person.year >= 19);
+console.log({ isAtLeast });
+console.log({ isEveryone });
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+const matchComment = comments.find((ele) => ele.id == 823423);
+console.log({ matchComment });
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+const commentIndex = comments.findIndex((ele) => ele.id == 823423);
+
+// const deletedComment = comments.splice(commentIndex, 1)[0];
+const deletedComment = comments.slice(commentIndex, commentIndex + 1)[0];
+
+const newCommentsArray = [
+    ...comments.slice(0, commentIndex),
+    ...comments.slice(commentIndex + 1),
+];
+
+console.log("Find the comment index with of 823423: ", commentIndex);
+console.log({ deletedComment });
+console.log("");
+console.log("Original comments array:");
+console.table(comments);
+console.log("");
+console.log("New comments array:");
+console.table(newCommentsArray);
