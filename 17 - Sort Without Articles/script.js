6b664a486f41795efe740de82e888df87c834f12
Ovidio Perez <oapm10@gmail.com>
2021-10-14 22:51:45 -0400

    Initiate project 17 (Sort Without Articles)

---
+++ 17 - Sort Without Articles/script.js
@@ -0,0 +1,39 @@
+/**Task
+ * se debe ordenar alfabeticamente
+ * se deben ignorar los "an", "the" y "a" iniciales al momento de ordenar
+ *
+ * Mostrar los elementos ordenados
+ */
+/** Ordenar Array
+ * usar .sort() function
+ * copiar el element
+ * identificar si hay un article ("an", "the" y "a") al inicio del string element a ordenar
+ *  podría usar .match() o .includes()
+ * identificar si el article está al principio
+ * realizar comparación con .localCompare() [leer más de localCompare]
+ *
+ * nota: probar usar .match() e .includes()
+ *
+ */
+
+/** Mostrar Array
+ * encerrar los elementos ordenados con li tags (usando map)
+ * juntar los elementos del array con join("")
+ * append el string dentro del UL
+ *
+ */
+const bands = [
+    "The Plot in You",
+    "The Devil Wears Prada",
+    "Pierce the Veil",
+    "Norma Jean",
+    "The Bled",
+    "Say Anything",
+    "The Midway State",
+    "We Came as Romans",
+    "Counterparts",
+    "Oh, Sleeper",
+    "A Skylit Drive",
+    "Anywhere But Here",
+    "An Old Dog",
+];
