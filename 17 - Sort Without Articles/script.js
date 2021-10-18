f8b2a1aa8e97d1b14c11a12569fa627ebe1aa3ff
Ovidio Perez <oapm10@gmail.com>
2021-10-18 17:53:25 -0400

    Finish project 17 (Sort Without Articles)

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -4,24 +4,6 @@
  *
  * Mostrar los elementos ordenados
  */
-/** Ordenar Array
- * usar .sort() function
- * copiar el element
- * identificar si hay un article ("an", "the" y "a") al inicio del string element a ordenar
- *  podría usar .match() o .includes()
- * identificar si el article está al principio
- * realizar comparación con .localCompare() [leer más de localCompare]
- *
- * nota: probar usar .match() e .includes()
- *
- */
-
-/** Mostrar Array
- * encerrar los elementos ordenados con li tags (usando map)
- * juntar los elementos del array con join("")
- * append el string dentro del UL
- *
- */
 const bands = [
     "The Plot in You",
     "The Devil Wears Prada",
@@ -37,3 +19,50 @@ const bands = [
     "Anywhere But Here",
     "An Old Dog",
 ];
+
+/** Ordenar Array
+ * usar .sort() function
+ *  copiar el element
+ *  identificar si hay un article ("an", "the" y "a") al inicio del string element a ordenar
+ *      usar .match()
+ *       ¿que hacer cuando match retorna 'null'?
+ *  identificar si el article está al principio
+ *
+ *  realizar comparación con .localCompare() [leer más de localCompare]
+ *
+ */
+console.log("Array: ");
+console.table(bands);
+
+bands.sort((bandA, bandB) => {
+    const regex = /(An|The|A)\s/i;
+
+    const foundA = deleteArticles(bandA, regex);
+    const foundB = deleteArticles(bandB, regex);
+
+    return foundA.localeCompare(foundB);
+});
+
+console.log("Alphabetically Sorted Array: ");
+console.table(bands);
+
+function deleteArticles(band, regex) {
+    if (band.match(regex) !== null && band.match(regex).index === 0) {
+        return band.replace(regex, "");
+    }
+    return band;
+}
+
+/** Mostrar Array
+ * encerrar los elementos ordenados con li tags (usando map)
+ * juntar los elementos del array con join("")
+ * append el string dentro del UL
+ */
+
+const bandList = document.getElementById("bands");
+const bandHtmlList = bands
+    .map((band) => {
+        return `<li>${band}</li>`;
+    })
+    .join("");
+bandList.innerHTML = bandHtmlList;
