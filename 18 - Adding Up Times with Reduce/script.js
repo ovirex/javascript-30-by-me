aa9657e91c3fcef21edc9ffb49322c5ec0301af3
Ovidio Perez <oapm10@gmail.com>
2021-10-20 00:01:32 -0400

    Finish project 18 (Adding Up Times with Reduce)

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -1,2 +1,24 @@
+/**Task:
+ * Mostrar las horas, minutos y segundos totales
+ * de la suma de los tiempos de todos los videos
+ */
+
 const videosList = Array.from(document.querySelectorAll(".videos [data-time]"));
-console.log(videosList);
+
+let total = videosList.reduce((acum, curr) => {
+    let [minutes, seconds] = curr.dataset.time.split(":").map(parseFloat);
+
+    // convert minutes to seconds
+    minutes *= 60;
+
+    return acum + minutes + seconds;
+}, 0);
+
+console.log(total);
+const hours = Math.floor(total / 3600);
+total -= hours * 3600;
+const minutes = Math.floor(total / 60);
+total -= minutes * 60;
+const seconds = total;
+
+console.log(hours, minutes, seconds);
